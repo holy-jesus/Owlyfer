c3f3ca8481ef23bb494cb5884e19de3be6b08225
holy-jesus <hj@holy-coder.ru>
2024-10-23 06:38:39 +0500

    Заменил `aiogram.fsm.storage.memory.MemoryStorage` на `aiogram.fsm.storage.mongo.MongoStorage`

--- utils/settings_loader.py
+++ utils/settings_loader.py
@@ -5,13 +5,17 @@ LOG_PATH = "logs/"
 SETTINGS_PATH = "data/settings.ini"
 DB_PATH = "data/database.db"
 
-#Загрузка настроек
+# Загрузка настроек
 try:
-    config = configparser.ConfigParser()
+    config = configparser.ConfigParser(allow_no_value=True)
     config.read(SETTINGS_PATH)
 
     BOT_TOKEN = config.get("telegram", "bot_token")
     CHANNEL_ID = config.get("telegram", "channel_id")
-except:
+    MONGODB_HOST = config.get("mongodb", "host", fallback="127.0.0.1")
+    MONGODB_PORT = int(config.get("mongodb", "port", fallback="27017"))
+    MONGODB_USERNAME = config.get("mongodb", "username") or None
+    MONGODB_PASSWORD = config.get("mongodb", "password") or None
+except Exception:
     print("Ошибка загрузки параметров, проверьте верность введенных данных")
     exit()
