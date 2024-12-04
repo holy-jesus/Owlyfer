92fe872db2bb1a5c3fdbdcf13656fb75dfcff94a
holy-jesus <hj@holy-coder.ru>
2024-12-05 04:56:28 +0500

    Заменил `aiogram.fsm.storage.memory.MemoryStorage` на `aiogram.fsm.storage.mongo.MongoStorage`

--- owlyfer/bot/loader.py
+++ owlyfer/bot/loader.py
@@ -1,15 +1,29 @@
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
+from aiogram.fsm.storage.mongo import MongoStorage
+from motor.motor_asyncio import AsyncIOMotorClient
 
 from utils.log_worker import Logger
 from utils.db_worker import DBWorker
-from utils.settings_loader import BOT_TOKEN
-
+from utils.settings_loader import (
+    BOT_TOKEN,
+    MONGODB_HOST,
+    MONGODB_PORT,
+    MONGODB_USERNAME,
+    MONGODB_PASSWORD,
+)
 
 try:
     bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-    dp = Dispatcher()
+    client = AsyncIOMotorClient(
+        host=MONGODB_HOST,
+        port=MONGODB_PORT,
+        username=MONGODB_USERNAME,
+        password=MONGODB_PASSWORD,
+        connect=True,
+    )
+    dp = Dispatcher(storage=MongoStorage(client=client, db_name="owlyfer"))
     db = DBWorker()
 except Exception as ex:
     Logger.error(ex)
--- owlyfer/utils/settings_loader.py
+++ owlyfer/utils/settings_loader.py
@@ -7,11 +7,15 @@ DB_PATH = "data/database.db"
 
 #Загрузка настроек
 try:
-    config = configparser.ConfigParser()
+    config = configparser.ConfigParser(allow_no_value=True)
     config.read(SETTINGS_PATH)
 
     BOT_TOKEN = config.get("telegram", "bot_token")
     CHANNEL_ID = config.get("telegram", "channel_id")
+    MONGODB_HOST = config.get("mongodb", "host", fallback="127.0.0.1")
+    MONGODB_PORT = int(config.get("mongodb", "port", fallback="27017"))
+    MONGODB_USERNAME = config.get("mongodb", "username") or None
+    MONGODB_PASSWORD = config.get("mongodb", "password") or None
 except:
     print("Ошибка загрузки параметров, проверьте верность введенных данных")
     exit()
