e0044066c51cee65230869d1f18a39ced07dd7a4
holy-jesus <hj@holy-coder.ru>
2024-12-05 03:46:33 +0500

    Перенёс бота в папку `owlyfer`

--- owlyfer/bot/handlers/admins/__init__.py
+++ owlyfer/bot/handlers/admins/__init__.py
@@ -2,5 +2,5 @@ from . import posts_control_module
 from . import main_menu_module
 from . import base_messages_module
 from . import mass_send_module
-from . import admins_control_module
+from . import admin_control
 from . import ban_control_module
